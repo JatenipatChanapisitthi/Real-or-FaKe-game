2d6cfdc36b354d27e61a01e46c4026af56ce20a4
JatenipatChanapisitthi <jatenipat.garfield03@gmail.com>
2025-04-21 22:51:31 +0700

    fix a little bit eiei

--- src/app/wow/page.tsx
+++ src/app/wow/page.tsx
@@ -4,8 +4,8 @@ export default function GamePage() {
 
   return (
     <div className="flex flex-col items-center mt-10">
-        <h1>wow pages</h1>
-        <h1>jateni</h1>
+        <h1 className="text-2xl">wow pages</h1>
+        <h1 className="text-9xl border-[90px] border-red-600 rounded-2xl ">asdfasdf</h1>
     </div>
   )
 }
--- src/components/FirstPage.tsx
+++ src/components/FirstPage.tsx
@@ -27,6 +27,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center mt-10">
+      <h1 className='text-4xl pb-10'>the chameleon game</h1>
       <h1 className="text-2xl mb-4">Enter Player Names</h1>
       <p>{nameList.length}</p>
       <div className="flex gap-2 mb-4">
@@ -40,11 +41,14 @@ export default function Home() {
           Add
         </button>
       </div>
-      <ul className="mb-4">
+      <div className='flex gap-1 mb-1'>
+      <ul className="mb-4 border border-fuchsia-600 p-2 pt-0.5 pb-0.5">
         {nameList.map((name, index) => (
           <li key={index}>{name}</li>
         ))}
       </ul>
+        <button className='border border-b-red-700 pr-2'>x</button>
+      </div>
       <button className="border border-blue-500 p-2" onClick={goToGame}>
         Start Game
       </button>
