e3cff38c6e14541b1305f70798d5b4e1ad0437dd
TAHPAPANGKORN <ththazach@gmail.com>
2025-04-25 21:50:48 +0700

    feat: same name detect

--- src/components/firstPage/FirstPage.tsx
+++ src/components/firstPage/FirstPage.tsx
@@ -14,6 +14,7 @@ export default function Home() {
   const [isEditing, setIsEditing] = useState(false);
   const [editName, setEditName] = useState("");
   const [editNameIndex, setEditNameIndex] =  useState<number | null>(null);
+  const [isSameName, setIsSameName] = useState(false);
 
   const router = useRouter()
 
@@ -29,6 +30,10 @@ export default function Home() {
       if (inputName.trim() !== '' && !nameList.includes(inputName)) {
         setNameList([...nameList, inputName.trim()])
         setInputName('')
+        setIsSameName(false)
+      }
+      else{
+        setIsSameName(true)
       }
 
   }
@@ -42,14 +47,17 @@ const handleEditClick = (index: number) => {
   setEditName(nameList[index]); 
   setEditNameIndex(index); 
 }
-
 const saveEditedWord = (index: number) => {
-  if (editName.trim() !== '' && !nameList.includes(editName.trim())) {
+  if (editName.trim() !== '' && !nameList.includes(editName.trim()) || nameList[index] === editName.trim()) {
     const updatedNameList = [...nameList];
     updatedNameList[index] = editName.trim(); 
     setNameList(updatedNameList);
     setIsEditing(false); 
     setEditName(''); 
+    setIsSameName(false)
+  }
+  else{
+    setIsSameName(true)
   }
 }
 
@@ -58,6 +66,8 @@ const handleCancelClick = () => {
   setEditName('')
   setEditNameIndex(null)
 }
+
+
 
 
   console.log(nameList)
@@ -88,12 +98,15 @@ const handleCancelClick = () => {
             onKeyDown={addNameKeyboard}
           />
           <button
-            className="bg-green-500 w-20 rounded-sm text-white"
+            className={`bg-green-500 hover:bg-green-400 w-20 rounded-sm text-white cursor-pointer`}
             onClick={addNameClick}
           >
             ADD
           </button>
         </div>
+          {isSameName && (
+            <p className='text-red-500 text-center pt-2'>SAME NAME!!</p>
+          )}
       </div>
 
       <div className="flex flex-col  gap-2 border border-black/40 rounded-sm p-5 w-90 md:w-120 ">
@@ -105,7 +118,7 @@ const handleCancelClick = () => {
         <ul className="mb-4 flex flex-col gap-2">
           {nameList.map((name, index) => (
             <div key={index} className="flex items-center gap-2">
-              <li className="bg-black/10 rounded-sm p-2 w-full flex items-center justify-center text-xl font-bold">
+              <li className={` bg-black/10 rounded-sm p-2 w-full flex items-center justify-center text-xl font-bold`}>
                 {isEditing && editNameIndex === index ? (
                   <input
                     type="text"
@@ -117,13 +130,13 @@ const handleCancelClick = () => {
                     className="border border-black/50 rounded-sm w-full h-full  text-xl p-2"
                   />
                 ) : (
-                  <p className='text-xl p-2'>{name}</p>
+                  <p className={`text-xl p-2`}>{name}</p>
                 )}
               </li>
               {isEditing && editNameIndex === index ?( 
               <div className="flex gap-2 ">
                 <button
-                  className="bg-green-400 w-14 h-14 rounded-[2px] text-white text-xl flex items-center justify-center cursor-pointer"
+                  className="bg-green-500 hover:bg-green-400 w-14 h-14 rounded-[2px] text-white text-xl flex items-center justify-center cursor-pointer"
                   onClick={() => {
                     saveEditedWord(index);
                   }}
@@ -142,7 +155,7 @@ const handleCancelClick = () => {
                ) : (
                 <div className="flex gap-2 ">
                 <button
-                  className="bg-blue-400 w-14 h-14 rounded-[2px] text-white text-xl flex items-center justify-center cursor-pointer"
+                  className="bg-blue-400 w-14 h-14 rounded-[2px] text-white text-xl flex items-center justify-center cursor-pointer hover:bg-blue-300"
                   onClick={() => {
                     handleEditClick(index);
                   }}
@@ -167,7 +180,7 @@ const handleCancelClick = () => {
           // className="bg-green-500 p-2 text-white font-bold"
           className={`${
             nameList.length >= 3
-              ? "bg-green-500  text-white cursor-pointer"
+              ? "bg-green-500  text-white cursor-pointer hover:bg-green-400"
               : "pointer-events-none opacity-50 border border-gray-300"
           } p-2  font-bold`}
           onClick={goToGame}
