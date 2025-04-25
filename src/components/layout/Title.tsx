e72e39350b0e4bfd21d45c67659569caa9a2a6f1
TAHPAPANGKORN <ththazach@gmail.com>
2025-04-25 21:55:55 +0700

    add: title

---
+++ src/components/layout/Title.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div className='flex flex-col items-center justify-center p-10'>
+      <h1 className='text-[34px] md:text-[48px] font-bold text-center'>The Chameleon Game</h1>
+      <p className='text-[15px] md:text-[14px] text-black/60 text-center '>Create by Jatenipat Chanapisitthi  and  Papangkorn Pitjawong</p>
+    </div>
+  )
+}
+
+export default Title
