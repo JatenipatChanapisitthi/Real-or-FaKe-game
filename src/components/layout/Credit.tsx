056771492a1eb10d64d062788f49060c509d2efa
JatenipatChanapisitthi <jatenipat.garfield03@gmail.com>
2025-04-27 04:41:50 +0700

    fix name jate credit 04:00

--- src/components/layout/Credit.tsx
+++ src/components/layout/Credit.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
 
     return (
       <div className="flex bg-[#FBFBFB] justify-center p-2 gap-2">
-        <p className="text-sm  text-gray-400">Create By Jatenipat Chanapisiti </p>
+        <p className="text-sm  text-gray-400">Create By Jatenipat Chanapisitthi </p>
         <p className="text-sm  text-gray-400">Papangkorn Pitjawong</p>
       </div>
     );
