d62635cf4b6726f32a24c3cfd56dc8edd65f8b8f
TAHPAPANGKORN <ththazach@gmail.com>
2025-08-11 14:20:55 +0700

    fix(layout): layout structure and aesthetics
    - improve layout with flexbox for better content distribution
    - add main tag for semantic clarity and overflow control
    - update metadata for accurate title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import Footer from "@/components/layout/Credit";
-import Title from '@/components/layout/Title'
+import Title from "@/components/layout/Title";
 import "./globals.css";
-import { Toaster } from 'sonner'
+import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  title: "Real of Fake Game",
-  description: "crete game offline game of party",
+  title: "Real or Fake Game",
+  description: "Create offline party game",
 };
 
 export default function RootLayout({
@@ -17,11 +17,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="vsc-initialized">
-        <Title />
-        <div className="min-h-screen overflow-hidden">{children}</div>
-        <Toaster position="top-center" richColors />
-        <Footer />
+        <div className="flex flex-col min-h-screen overflow-hidden">
+          <Toaster position="top-center" richColors />
+          <Title />
+          <main className="flex-grow overflow-auto">{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
 }
+
